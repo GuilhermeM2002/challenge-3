660972bdd6b7dd4a7d1ff7809311ec2b0c8ae1e5
guilhermemr <guilherme.infor2@gmail.com>
2024-10-10 18:10:26 -0300

    first commit

---
+++ OrderIngestionService/src/main/kotlin/br/com/challenge3/OrderIngestionService/OrderIngestionServiceApplication.kt
@@ -0,0 +1,11 @@
+package br.com.challenge3.OrderIngestionService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderIngestionServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderIngestionServiceApplication>(*args)
+}
---
+++ OrderIngestionService/src/test/kotlin/br/com/challenge3/OrderIngestionService/OrderIngestionServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.challenge3.OrderIngestionService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderIngestionServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
